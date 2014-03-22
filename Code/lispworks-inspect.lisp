a4248c0d93a93f3058b5aa6b79d871a2fe6cb74a
Scott L. Burson <Gyro@bountyoss.com>
2014-03-22 13:53:11 -0700

    Rewrote Cam's code to use GMAP instead of LOOP.

--- Code/lispworks-inspect.lisp
+++ Code/lispworks-inspect.lisp
@@ -4,48 +4,36 @@
   nil)
 
 (defmethod lw:get-inspector-values ((object set) mode)
-  (values
-   (loop for count from 0 below (size object)
-         collecting count)
-   (convert 'list object)
-   nil
-   nil
-   "SET"))
+  (values (gmap :list #'identity (:index 0 (size object)))
+	  (convert 'list object)
+	  nil
+	  nil
+	  "SET"))
 
 (defmethod lw:get-inspector-values ((object bag) mode)
-  (values
-   (let (names)
-     (do-bag-pairs (value mult object names)
-       (lw:appendf names (list value))))
-   (let (values)
-     (do-bag-pairs (value mult object values)
-       (lw:appendf values (list mult))))
-   nil
-   nil
-   "BAG"))
+  (values (gmap :list (fn (x _y) x) (:bag-pairs object))
+	  (gmap :list (fn (_x y) y) (:bag-pairs object))
+	  nil
+	  nil
+	  "BAG"))
 
 (defmethod lw:get-inspector-values ((object map) mode)
-  (values
-   (convert 'list (domain object))
-   (convert 'list (range object))
-   nil
-   nil
-   "MAP"))
+  (values (convert 'list (domain object))
+	  (convert 'list (range object))
+	  nil
+	  nil
+	  "MAP"))
 
 (defmethod lw:get-inspector-values ((object map) (mode (eql 'domain)))
-  (values
-   (loop for count from 0 below (size object)
-         collecting count)
-   (convert 'list (domain object))
-   nil
-   nil
-   "MAP-DOMAIN"))
+  (values (gmap :list #'identity (:index 0 (size object)))
+	  (convert 'list (domain object))
+	  nil
+	  nil
+	  "MAP-DOMAIN"))
 
 (defmethod lw:get-inspector-values ((object seq) mode)
-  (values
-   (loop for count from 0 below (size object)
-         collecting count)
-   (convert 'list object)
-   nil
-   nil
-   "SEQ"))
+  (values (gmap :list #'identity (:index 0 (size object)))
+	  (convert 'list object)
+	  nil
+	  nil
+	  "SEQ"))
