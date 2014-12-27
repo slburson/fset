slburson/fset

commit ecce2366f274921f402e232d13518ac9a71f9bf6

Tweak the interface of RANK, and export it and AT-RANK.  (WARNING: these are only lightly tested.)

Code/defs.lisp
@@ -43,7 +43,7 @@
 	   #:empty-set #:empty-bag #:empty-map #:empty-seq #:empty-tuple
 	   #:empty-wb-set #:empty-wb-bag #:empty-wb-map #:empty-wb-seq
 	   #:empty-dyn-tuple
-	   #:least #:greatest #:lookup #:@
+	   #:least #:greatest #:lookup #:rank #:at-rank #:@
 	   #:with #:less #:split-from #:split-above #:split-through #:split-below
 	   #:union #:bag-sum #:intersection #:bag-product #:complement
 	   #:set-difference #:set-difference-2 #:bag-difference

Code/fset.lisp
@@ -108,17 +108,17 @@ as two values, else false and `nil'; this is useful for canonicalization."))
 
 (defgeneric rank (collection value)
   (:documentation
-    "If `collection' is a set or bag that contains `value', returns the rank of
-`value' in the ordering defined by `compare', and a true second value.  If
-`collection' is a map whose domain contains `value', returns the rank of
-`value' in the domain of the map, and a true second value.  If `value' is not
-in the collection, the second value is false, and the first value is the rank
-of the least member of the collection greater than `value' (if any; otherwise
-the size (for a bag, the set-size) of the collection).  Note that if there are
-values/keys that are unequal but equivalent to `value', an arbitrary order
-will be imposed on them for this purpose; but another collection that is
-`equal?' but not `eq' to this one will in general order them differently.
-Also, on a bag, multiplicities are ignored for this purpose."))
+    "If `collection' is a set or bag that contains `value', returns the rank
+of `value' in the ordering defined by `compare', and a true second value.
+If `collection' is a map whose domain contains `value', returns the rank of
+`value' in the domain of the map, and a true second value.  If `value' is
+not in the collection, the second value is false, and the first value is the
+rank of the greatest member of the collection less than `value' (if any;
+otherwise zero).  Note that if there are values/keys that are unequal but
+equivalent to `value', an arbitrary order will be imposed on them for this
+purpose; but another collection that is `equal?' but not `eq' to this one
+will in general order them differently.  Also, on a bag, multiplicities are
+ignored for this purpose."))
 
 (defgeneric at-rank (collection rank)
   (:documentation
@@ -1044,7 +1044,7 @@ the default implementation of sets in FSet."
 
 (defmethod rank ((s wb-set) x)
   (let ((found? rank (WB-Set-Tree-Rank (wb-set-contents s) x)))
-    (values rank found?)))
+    (values (if found? rank (1- rank)) found?)))
 
 (defmethod at-rank ((s wb-set) rank)
   (let ((contents (wb-set-contents s))
@@ -1469,7 +1469,7 @@ trees.  This is the default implementation of bags in FSet."
 
 (defmethod rank ((s wb-bag) x)
   (let ((found? rank (WB-Bag-Tree-Rank (wb-bag-contents s) x)))
-    (values rank found?)))
+    (values (if found? rank (1- rank)) found?)))
 
 (defmethod at-rank ((s wb-bag) rank)
   (let ((contents (wb-bag-contents s))
@@ -1988,7 +1988,7 @@ the default implementation of maps in FSet."
 
 (defmethod rank ((s wb-map) x)
   (let ((found? rank (WB-Map-Tree-Rank (wb-map-contents s) x)))
-    (values rank found?)))
+    (values (if found? rank (1- rank)) found?)))
 
 (defmethod at-rank ((s wb-map) rank)
   (let ((contents (wb-map-contents s))

commit 8e25705f7eb6bb4a10813a12a6a2047ec6dea0dc

Replace implementation-dependent code to seed the random-state with portable library MT19937.
Also, fix test cases for `rank` that I broke in ecce236.

Code/defs.lisp
@@ -14,6 +14,7 @@
 (defpackage :fset
   (:use :cl :gmap :new-let :lexical-contexts)
   (:shadowing-import-from :new-let #:let #:cond)
+  (:shadowing-import-from :mt19937 #:make-random-state #:random)
   ;; For each of these shadowed symbols, using packages must either shadowing-
   ;; import it or shadowing-import the original Lisp symbol.
   (:shadow ;; Shadowed type/constructor names

Code/fset.asd
@@ -12,7 +12,7 @@
 ;;; This license provides NO WARRANTY.
 
 (defsystem FSet
-  :depends-on (:gmap)
+  :depends-on (:gmap :mt19937)
   :components
   ((:file "defs")
    (:file "port" :depends-on ("defs"))

Code/fset.lisp
@@ -113,7 +113,7 @@ If `collection' is a map whose domain contains `value', returns the rank of
 `value' in the domain of the map, and a true second value.  If `value' is
 not in the collection, the second value is false, and the first value is the
 rank of the greatest member of the collection less than `value' (if any;
-otherwise zero).  Note that if there are values/keys that are unequal but
+otherwise -1).  Note that if there are values/keys that are unequal but
 equivalent to `value', an arbitrary order will be imposed on them for this
 purpose; but another collection that is `equal?' but not `eq' to this one
 will in general order them differently.  Also, on a bag, multiplicities are

Code/port.lisp
@@ -289,34 +289,6 @@
 
 ;;; ----------------
 
-;;; Unfortunately, CL doesn't specify that `make-random-state' should be able
-;;; to accept an integer seed.  We want to be able to supply it one, so that
-;;; (for testing) we can have multiple reproducible sequences of pseudorandom
-;;; numbers.  (May not be possible on all implementations.)
-(defun make-seeded-random-state (seed)
-  (if (null seed)
-      (make-random-state)
-    #+(or cmu scl)
-    (progn
-      (assert (plusp seed))
-      (kernel::make-random-object :state
-				  (kernel::init-random-state (logand seed #xFFFFFFFF))))
-    #+sbcl
-    (progn
-      (assert (plusp seed))
-      (sb-kernel::%make-random-state
-	:state (sb-kernel::init-random-state (logand seed #xFFFFFFFF))))
-    #+openmcl
-    (ccl::initialize-random-state (ash (logand seed #xFFFFFFFF) -16)
-				  (logand seed #xFFFF))
-    #+genera
-    (fcli::make-random-state-internal 71 35 seed)
-    #-(or cmu scl sbcl openmcl genera)
-    (error "Implementation-specific code needed in `make-seeded-random-state'")))
-
-
-;;; ----------------
-
 #-lispworks
 (defun base-char-p (x)
   (typep x 'base-char))

Code/testing.lisp
@@ -29,7 +29,7 @@
 
 (defun run-test-suite (n-iterations &optional random-seed)
   (Test-Misc)
-  (let ((*random-state* (make-seeded-random-state random-seed))) ; for repeatability.
+  (let ((*random-state* (make-random-state random-seed))) ; for repeatability.
     (dotimes (i n-iterations)
       (Test-Map-Operations i (Test-Set-Operations i))
       (Test-Bag-Operations i)
@@ -200,27 +200,27 @@
       (test (let ((rank val? (rank (set 1 2 3 4) 2)))
 	      (and (= rank 1) val?)))
       (test (let ((rank val? (rank (set 1 2 3 4) 3.5)))
-	      (and (= rank 3) (not val?))))
+	      (and (= rank 2) (not val?))))
       (test (let ((rank val? (rank (set 1 2 3 4) 5)))
-	      (and (= rank 4) (not val?))))
+	      (and (= rank 3) (not val?))))
       (test (let ((rank val? (rank (set) 5)))
-	      (and (= rank 0) (not val?))))
+	      (and (= rank -1) (not val?))))
       (test (let ((rank val? (rank (bag 1 2 3 4) 2)))
 	      (and (= rank 1) val?)))
       (test (let ((rank val? (rank (bag 1 2 3 4) 3.5)))
-	      (and (= rank 3) (not val?))))
+	      (and (= rank 2) (not val?))))
       (test (let ((rank val? (rank (bag 1 2 3 4) 5)))
-	      (and (= rank 4) (not val?))))
+	      (and (= rank 3) (not val?))))
       (test (let ((rank val? (rank (bag) 5)))
-	      (and (= rank 0) (not val?))))
+	      (and (= rank -1) (not val?))))
       (test (let ((rank val? (rank (map (1 12) (2 27) (3 39) (4 46)) 2)))
 	      (and (= rank 1) val?)))
       (test (let ((rank val? (rank (map (1 12) (2 27) (3 39) (4 46)) 3.5)))
-	      (and (= rank 3) (not val?))))
+	      (and (= rank 2) (not val?))))
       (test (let ((rank val? (rank (map (1 12) (2 27) (3 39) (4 46)) 5)))
-	      (and (= rank 4) (not val?))))
+	      (and (= rank 3) (not val?))))
       (test (let ((rank val? (rank (map) 5)))
-	      (and (= rank 0) (not val?))))
+	      (and (= rank -1) (not val?))))
       (test (eql (at-rank (set 4 8 2 3 6) 3) 6))
       (test (eql (at-rank (bag 4 8 2 4 3 2 6) 3) 6))
       (test (let ((key val (at-rank (map ('a 3) ('d 7) ('c 3) ('g 1) ('e 6)) 3)))
@@ -365,11 +365,11 @@
 		 ((not (contains? fs0 r)) r))))
       (unless (= (rank fs0 r)
 		 (if (greater-than? r (greatest fs0))
-		     (size fs0)
+		     (1- (size fs0))
 		   (do ((r2 r (1+ r2)))
 		       ((contains? fs0 r2)
-			(rank fs0 r2)))))
-	(error "Set at-rank of non-member failed")))
+			(1- (rank fs0 r2))))))
+	(error "Set rank of non-member failed")))
     fs0))
 
 
@@ -513,11 +513,11 @@
 		 ((not (domain-contains? fm0 r)) r))))
       (unless (= (rank fm0 r)
 		 (if (greater-than? r (greatest fm0))
-		     (size fm0)
+		     (1- (size fm0))
 		   (do ((r2 r (1+ r2)))
 		       ((contains? fm0 r2)
-			(rank fm0 r2)))))
-	(error "Map at-rank of non-member failed")))))
+			(1- (rank fm0 r2))))))
+	(error "Map rank of non-member failed")))))
 
 
 (defun Test-Bag-Operations (i)
@@ -658,11 +658,11 @@
 		 ((not (contains? fb0 r)) r))))
       (unless (= (rank fb0 r)
 		 (if (greater-than? r (greatest fb0))
-		     (set-size fb0)
+		     (1- (set-size fb0))
 		   (do ((r2 r (1+ r2)))
 		       ((contains? fb0 r2)
-			(rank fb0 r2)))))
-	(error "Bag at-rank of non-member failed")))
+			(1- (rank fb0 r2))))))
+	(error "Bag rank of non-member failed")))
     fb0))
 
 

fset.asd
@@ -11,11 +11,11 @@
 
 (asdf:defsystem FSet
   :description "A functional set-theoretic collections library.
-See: http://common-lisp.net/project/fset/
+See: http://www.ergy.com/FSet.html
 "
-  :version "1.3.0"
+  :version "1.3.1"
   :licence "LLGPL"
-  :depends-on (:misc-extensions)
+  :depends-on (:misc-extensions :mt19937)
   :serial t
   :components
   ((:module "Code"