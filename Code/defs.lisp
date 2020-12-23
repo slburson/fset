e73e289bc31e80cc4fa56158b1388599ebf748d9
Bahman Movaqar <bahman.movaqar@hootsuite.com>
2020-12-23 14:45:29 -0800

    export all shadowed symbols
    re #34, added the symbols `some`, `every`, `notany` and `notevery`,
    which are already shadowed, to the export list.

--- Code/defs.lisp
+++ Code/defs.lisp
@@ -64,7 +64,8 @@
 	   #:position #:position-if #:position-if-not
 	   #:remove #:remove-if #:remove-if-not
 	   #:substitute #:substitute-if #:substitute-if-not
-	   #:convert #:iterator 
+           #:some #:every #:notany #:notevery
+	   #:convert #:iterator
 	   #:do-set #:do-bag #:do-bag-pairs #:do-map #:do-map-domain #:do-seq #:do-tuple
 	   #:adjoinf #:removef #:includef #:excludef
 	   #:unionf #:intersectf #:imagef #:composef
