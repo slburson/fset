slburson/fset

commit fe583ba9de8ccb140797ad4bbd3b729506a18c6a

Fix to previous, and more cleanup.

Code/macros.lisp
@@ -172,14 +172,11 @@ However, the implementation tries very hard to prevent this."
 	((prev-types (cdr types))))
     `(progn
        (let ((mto-len (length +Master-Type-Ordering+)))
-	 (unless (if (< mto-len ,(length types))
-		     (equal +Master-Type-Ordering+
-			    (cl:subseq ',prev-types (- ,(length prev-types) mto-len)))
-		   (equal (cl:subseq +Master-Type-Ordering+
-				     (- mto-len ,(length types)))
-			  ',types))
+	 (unless (equal (cl:subseq +Master-Type-Ordering+
+				   (- mto-len ,(length types)))
+			',types)
 	   ;; This can happen if calls to this macro are compiled in a different
-	   ;; order on different occasions, but only if neither call has been loaded.
+	   ;; order on different occasions.
 	   (error "FSet master type ordering out of sync.~@
 		   See fset::define-cross-type-compare-methods.")))
        (unless (member ',type +Master-Type-Ordering+)

Code/port.lisp
@@ -275,13 +275,15 @@
 
 (defmacro define-atomic-series (name &optional doc-string)
   #+(and sbcl 64-bit)
-  `(sb-ext:defglobal ,name 0 ,(and doc-string `(,doc-string)))
+  `(progn
+     (sb-ext:defglobal ,name 0 . ,(and doc-string `(,doc-string)))
+     (declaim (type fixnum ,name)))
   #-(and sbcl 64-bit)
   `(deflex ,name (cons 0 (make-lock ',lock-name)) . ,(and doc-string `(,doc-string))))
 
 (defmacro increment-atomic-series (name)
   #+(and sbcl 64-bit)
-  `(1- (sb-ext:atomic-incf ,name))
+  `(sb-ext:atomic-incf ,name) ; (postincrement)
   #+(and allegro smp-macros)
   `(1- (excl:incf-atomic (car ,name)))
   #-(or (and sbcl 64-bit) (and allegro smp-macros))

commit 05676f9928452e9d36b0eebdf15f31926e1afcea

Oops -- revert breakage in fe583ba.

Code/macros.lisp
@@ -172,9 +172,12 @@ However, the implementation tries very hard to prevent this."
 	((prev-types (cdr types))))
     `(progn
        (let ((mto-len (length +Master-Type-Ordering+)))
-	 (unless (equal (cl:subseq +Master-Type-Ordering+
-				   (- mto-len ,(length types)))
-			',types)
+	 (unless (if (< mto-len ,(length types))
+		     (equal +Master-Type-Ordering+
+			    (cl:subseq ',prev-types (- ,(length prev-types) mto-len)))
+		   (equal (cl:subseq +Master-Type-Ordering+
+				     (- mto-len ,(length types)))
+			  ',types))
 	   ;; This can happen if calls to this macro are compiled in a different
 	   ;; order on different occasions.
 	   (error "FSet master type ordering out of sync.~@