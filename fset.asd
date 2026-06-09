slburson/fset

commit 984cca91523a7eb0b4578d0ee3e3b0cd252671ee

More Modern-CL: replay sets/maps, 2-relations.

fset.asd
@@ -40,7 +40,7 @@ See: https://gitlab.common-lisp.net/fset/fset/-/wikis/home
 	     (:file "complement-sets")
 	     (:file "bounded-sets")
 	     (:file "transients")
-	     (:file "testing")
+	     (:file "testing-0")
 	     (:file "testing-1")
 	     (:file "post")
 	     #+swank

Code/reader.lisp
@@ -965,7 +965,9 @@ contains the pairs <1, a>, <1, b>, <2, a>, and <2, b>."
 
 (defmacro ch-custom-2-relation (key-compare-fn-name val-compare-fn-name &rest args)
   "Constructs a ch-2-relation with a custom ordering, according to the supplied
-argument subforms.  Each argument subform can be a list of the form \(`key-expr'
+argument subforms.  `key-compare-fn-name' and `val-compare-fn-name' must be
+symbols naming the comparison functions to be used for keys and values
+respectively.  Each argument subform can be a list of the form \(`key-expr'
 `value-expr'\), denoting a mapping from the value of `key-expr' to the value of
 `value-expr'; or a list of the form ($ `expression'), in which case the
 expression must evaluate to a 2-relation, all of whose mappings will be included

Code/relations.lisp
@@ -20,10 +20,10 @@
 (defgeneric arity (rel)
   (:documentation "Returns the arity of the relation `rel'."))
 
-(defgeneric join (relation-a column-a relation-b column-b)
+(defgeneric join (relation-1 column-1 relation-2 column-2)
   (:documentation
-    "A relational equijoin, matching up `column-a' of `relation-a' with `column-b' of
-`relation-b'.  For a binary relation, the columns are named 0 (domain) and 1 (range)."))
+    "A relational equijoin, matching up `column-1' of `relation-1' with `column-2' of
+`relation-2'.  For a binary relation, the columns are named 0 (domain) and 1 (range)."))
 
 (define-cross-type-compare-methods relation)
 
@@ -65,6 +65,20 @@ is mapped to multiple range values."))
     (do-2-relation (x y rel1)
       (when (contains? rel2 x y)
 	(includef result x y)))
+    result))
+
+(defmethod join ((rel1 2-relation) col1 (rel2 2-relation) col2)
+  (let ((rel1 (ecase col1
+		(1 rel1)
+		(0 (inverse rel1))))
+	(rel2 (ecase col2
+		(0 rel2)
+		(1 (inverse rel2))))
+	((result (empty-relation-like rel1 :key-compare-fn-name (key-compare-fn-name rel1)
+					   :val-compare-fn-name (val-compare-fn-name rel2)))))
+    (do-2-relation (x y rel1)
+      (do-set (z (lookup rel2 y))
+	(includef result x z)))
     result))
 
 
@@ -154,9 +168,16 @@ values."
 	      (make-wb-2-relation 0 nil 'no-inverse (make-tree-map-org key-compare-fn-name key-compare-fn
 								       val-compare-fn-name val-compare-fn)))))))
 
-(defmethod empty-relation-like ((rel wb-2-relation))
+(defmethod empty-relation-like ((rel wb-2-relation) &key key-compare-fn-name val-compare-fn-name)
   (let ((org (wb-2-relation-org rel)))
-    (empty-wb-2-relation (tree-map-org-key-compare-fn-name org) (tree-map-org-val-compare-fn-name org))))
+    (empty-wb-2-relation (or key-compare-fn-name (tree-map-org-key-compare-fn-name org))
+			 (or val-compare-fn-name (tree-map-org-val-compare-fn-name org)))))
+
+(defmethod key-compare-fn-name ((m wb-2-relation))
+  (tree-map-org-key-compare-fn-name (wb-2-relation-org m)))
+
+(defmethod val-compare-fn-name ((m wb-2-relation))
+  (tree-map-org-val-compare-fn-name (wb-2-relation-org m)))
 
 (defmethod empty? ((rel wb-2-relation))
   (zerop (wb-2-relation-size rel)))
@@ -176,6 +197,17 @@ values."
   (let ((org (wb-2-relation-org rel))
 	((found? set-tree (WB-Map-Tree-Lookup (wb-2-relation-map0 rel) x (tree-map-org-key-compare-fn org)))))
     (and found? (WB-Set-Tree-Contains? set-tree y (tree-map-org-val-compare-fn org)))))
+
+(defmethod domain-contains? ((rel wb-2-relation) x)
+  (let ((org (wb-2-relation-org rel)))
+    ;; Wrap in `(not (null ...))' so we don't expose the internal set tree.
+    (not (null (WB-Map-Tree-Lookup (wb-2-relation-map0 rel) x (tree-map-org-key-compare-fn org))))))
+
+(defmethod range-contains? ((rel wb-2-relation) x)
+  (wb-2-relation-get-inverse rel)
+  (let ((org (wb-2-relation-org rel)))
+    ;; Wrap in `(not (null ...))' so we don't expose the internal set tree.
+    (not (null (WB-Map-Tree-Lookup (wb-2-relation-map1 rel) x (tree-map-org-key-compare-fn org))))))
 
 ;;; Note that `(setf (lookup rel x) y)' is the same as `(includef rel x y)'.
 (define-methods (lookup fset2:lookup) ((rel wb-2-relation) x)
@@ -850,9 +882,16 @@ values."
 				  (make-hash-map-org key-compare-fn-name key-compare-fn key-hash-fn
 						     val-compare-fn-name val-compare-fn val-hash-fn)))))))
 
-(defmethod empty-relation-like ((rel ch-2-relation))
+(defmethod empty-relation-like ((rel ch-2-relation) &key key-compare-fn-name val-compare-fn-name)
   (let ((org (ch-2-relation-org rel)))
-    (empty-ch-2-relation (hash-map-org-key-compare-fn-name org) (hash-map-org-val-compare-fn-name org))))
+    (empty-ch-2-relation (or key-compare-fn-name (hash-map-org-key-compare-fn-name org))
+			 (or val-compare-fn-name (hash-map-org-val-compare-fn-name org)))))
+
+(defmethod key-compare-fn-name ((m ch-2-relation))
+  (hash-map-org-key-compare-fn-name (ch-2-relation-org m)))
+
+(defmethod val-compare-fn-name ((m ch-2-relation))
+  (hash-map-org-val-compare-fn-name (ch-2-relation-org m)))
 
 (defmethod empty? ((rel ch-2-relation))
   (zerop (ch-2-relation-size rel)))
@@ -873,6 +912,19 @@ values."
 	((found? set-tree (ch-map-tree-lookup (ch-2-relation-map0 rel) x
 					      (hash-map-org-key-hash-fn org) (hash-map-org-key-compare-fn org)))))
     (and found? (ch-set-tree-contains? set-tree y (hash-map-org-val-hash-fn org) (hash-map-org-val-compare-fn org)))))
+
+(defmethod domain-contains? ((rel ch-2-relation) x)
+  (let ((org (ch-2-relation-org rel)))
+    ;; Wrap in `(not (null ...))' so we don't expose the internal set tree.
+    (not (null (ch-map-tree-lookup (ch-2-relation-map0 rel) x
+				   (hash-map-org-key-hash-fn org) (hash-map-org-key-compare-fn org))))))
+
+(defmethod range-contains? ((rel ch-2-relation) x)
+  (ch-2-relation-get-inverse rel)
+  (let ((org (ch-2-relation-org rel)))
+    ;; Wrap in `(not (null ...))' so we don't expose the internal set tree.
+    (not (null (ch-map-tree-lookup (ch-2-relation-map1 rel) x
+				   (hash-map-org-key-hash-fn org) (hash-map-org-key-compare-fn org))))))
 
 (define-methods (lookup fset2:lookup) ((rel ch-2-relation) x)
   "Returns the set of values that the relation pairs `x' with."

Code/testing-1.lisp
@@ -1618,6 +1618,8 @@
                  '(1 2 t)))
     (test (contains? (wb-2-relation (1 2) (1 3) (4 7)) 1 2))
     (test (not (contains? (wb-2-relation (1 2)) 1 3)))
+    (test (domain-contains? (wb-2-relation (1 2) (1 3) (4 7)) 4))
+    (test (range-contains? (wb-2-relation (1 2) (1 3) (4 7)) 3))
     (test (equal? (lookup (empty-wb-2-relation) 1) (set)))
     (test (equal? (lookup (wb-2-relation (1 2)) 1) (set 2)))
     (test (equal? (lookup-inv (wb-2-relation (1 2)) 2) (set 1)))
@@ -1832,6 +1834,8 @@
                  '(1 2 t)))
     (test (contains? (ch-2-relation (1 2) (1 3) (4 7)) 1 2))
     (test (not (contains? (ch-2-relation (1 2)) 1 3)))
+    (test (domain-contains? (ch-2-relation (1 2) (1 3) (4 7)) 4))
+    (test (range-contains? (ch-2-relation (1 2) (1 3) (4 7)) 3))
     (test (equal? (lookup (empty-ch-2-relation) 1) (ch-set)))
     (test (equal? (lookup (ch-2-relation (1 2)) 1) (ch-set 2)))
     (test (equal? (lookup-inv (ch-2-relation (1 2)) 2) (ch-set 1)))
@@ -2013,7 +2017,19 @@
 
     (test (equal? (transitive-closure (ch-2-relation (1 3) (1 4) (3 7) (3 8) (4 11) (5 14) (6 12) (11 23))
 				      (ch-set 1))
-		  (ch-set 1 3 4 7 8 11 23)))))
+		  (ch-set 1 3 4 7 8 11 23)))
+
+    ;; Fallback methods
+    (test (equal? (union (ch-2-relation (1 2) (1 4)) (wb-2-relation (1 3) (2 5)))
+                  (ch-2-relation (1 2) (1 3) (1 4) (2 5))))
+    (test (equal? (intersection (ch-2-relation ('a 7) ('b 12) ('c 17)) (wb-2-relation ('b 12) ('c 17) ('c 22)))
+		  (ch-2-relation ('b 12) ('c 17))))
+    (test (equal? (join (ch-2-relation ('a 3) ('a 5) ('b 7) ('b 8)) 1
+			(wb-2-relation (3 "yo") (7 "eek") (7 "mrr")) 0)
+		  (ch-2-relation ('a "yo") ('b "eek") ('b "mrr"))))
+    (test (equal? (join (wb-2-relation (3 "mrr") (7 "eek") (7 "mrr")) 0
+			(ch-custom-2-relation 'compare 'erapmoc ('a 3) ('a 5) ('b 7) ('b 8)) 1)
+		  (wb-2-relation ("eek" 'b) ("mrr" 'a) ("mrr" 'b))))))
 
 (defun Test-List-Relations ()
   "Simple tests on List-Relations and Query-Registries."

Doc/Modern-CL/Top.texi
@@ -141,7 +141,7 @@ Iterating over FSet Collections
   GMap
   Iterate
   Others
-The @code{convert} Generic Function
+Generic Function @code{convert}
 Printing and Reading
   JSON
 Larger Examples

commit c94817f12680529ac34196b347510cd67ceb7fe1

test: fix testing filenames in asd
Regression introduced in 984cca91523a7eb0b4578d0ee3e3b0cd252671ee

fset.asd
@@ -54,7 +54,9 @@ See: https://gitlab.common-lisp.net/fset/fset/-/wikis/home
   :perform (test-op (o c) (symbol-call :fset :run-test-suite 200))
   :components
   ((:module "Code"
-	    :components ((:file "testing")))))
+	    :components
+	    ((:file "testing-0")
+	     (:file "testing-1")))))
 
 (defsystem :FSet/Iterate
   :description "FSet definitions for the Iterate macro."